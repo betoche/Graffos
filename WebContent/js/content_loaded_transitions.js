556305cbd53b7f343713ab058daf1c94e3da1620
lesther Morales Hudiel <lesther@inmobia.com>
2014-04-30 23:46:11 -0600

    adding parallax

--- WebContent/js/content_loaded_transitions.js
+++ WebContent/js/content_loaded_transitions.js
@@ -1,4 +1,5 @@
 window.onload = detectarCarga;
 	function detectarCarga(){
-	$(".trans").css("display","block")
+	$(".trans").css("display","block");
+	$(".trans_i").css("display","inline-block")
 }    
